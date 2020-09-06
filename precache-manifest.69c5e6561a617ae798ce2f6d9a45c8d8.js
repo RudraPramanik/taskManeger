9a249e840247d98a819e678f1d2496daffb698ff
rudra pramanik <rudrahimangshoo2020@gmail.com>
2020-09-06 23:21:51 +0600

    Updates

---
+++ precache-manifest.69c5e6561a617ae798ce2f6d9a45c8d8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0e38557f9730baa24e7da38ef44fec42",
+    "url": "/RudraPramanik/taskManeger/index.html"
+  },
+  {
+    "revision": "082b54e5352e05be1e08",
+    "url": "/RudraPramanik/taskManeger/static/css/main.2b9ab736.chunk.css"
+  },
+  {
+    "revision": "80ccdfe89238c4d39f99",
+    "url": "/RudraPramanik/taskManeger/static/js/2.b566954d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/RudraPramanik/taskManeger/static/js/2.b566954d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "082b54e5352e05be1e08",
+    "url": "/RudraPramanik/taskManeger/static/js/main.d9a9fc14.chunk.js"
+  },
+  {
+    "revision": "98232b6def5f9ed1867b",
+    "url": "/RudraPramanik/taskManeger/static/js/runtime-main.543fa4a6.js"
+  }
+]);
